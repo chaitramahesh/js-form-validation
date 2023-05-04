85d5449b82c097b3f2f3358fff6fadba90f13715
chaitramahesh <chaitrah10@gmail.com>
2023-05-04 18:36:15 +0530

    added event to submit button

--- script.js
+++ script.js
@@ -48,4 +48,8 @@ let password = document.forms['myForm']["fpassword"].value;
    }
 }
 
-
+let button = document.getElementsByClassName("button")[0]
+console.log(button)
+button.addEventListener('click', (e)=>{
+    console.log(e)
+})
